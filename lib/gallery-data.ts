91a6b3d29b2c2960b39b00730e450da976111d78
LynkRookie <lynkrookie@gmail.com>
2026-06-19 17:29:23 -0400

    Update gallery-data.ts

--- lib/gallery-data.ts
+++ lib/gallery-data.ts
@@ -351,9 +351,9 @@ export const albums: Album[] = [
           // Índice 0 aquí = foto2 (la foto1 ya está separada arriba con isIntro).
           // Para agregar más, copia el formato: 5: "Tu mensaje aquí",
           56:  "Que tema más entretenido con mis hijos, nuestras viejas nos perdonarán que las engañamos",
-          117: "Papá, tu ausencia duele cada día, pero tu amor, tus enseñanzas y tu recuerdo siguen iluminando mi vida desde el cielo.\nTe extraño con toda mi alma y te amaré por siempre, esperando el día en que podamos abrazarnos nuevamente y estar juntos otra vez.",
-          154: "Te amo hasta el infinito y más allá, me haces mucha falta Papá, gracias por tanto y por todo.",
-          216: "Pronto volveremos a estar juntos, mi vieja hermosa. Yo estoy bien; cuida y aconseja a nuestros hijos, y entrega todo tu cariño a nuestros nietos y bisnietos, porque desde aquí también los acompaño y los cuido cada día. A todos los amo con toda mi alma, y a ti, mi vieja querida, te amo más de lo que las palabras pueden expresar.",
+          118: "Papá, tu ausencia duele cada día, pero tu amor, tus enseñanzas y tu recuerdo siguen iluminando mi vida desde el cielo.\nTe extraño con toda mi alma y te amaré por siempre, esperando el día en que podamos abrazarnos nuevamente y estar juntos otra vez.",
+          155: "Te amo hasta el infinito y más allá, me haces mucha falta Papá, gracias por tanto y por todo.",
+          217: "Pronto volveremos a estar juntos, mi vieja hermosa. Yo estoy bien; cuida y aconseja a nuestros hijos, y entrega todo tu cariño a nuestros nietos y bisnietos, porque desde aquí también los acompaño y los cuido cada día. A todos los amo con toda mi alma, y a ti, mi vieja querida, te amo más de lo que las palabras pueden expresar.",
 
         },
         0 // musicSection 0 = canciones del abuelo
@@ -367,7 +367,7 @@ export const albums: Album[] = [
       // ═══════════════���════════════════════════════════════════
       {
         type: "dedicatoria",
-        text: "Ahora que nuestro padre no está con nosotros,\nusted es nuestro madre y padre,\nen el cual nosotros la queremos\ny amamos todos los días.",
+        text: "Ahora que nuestro padre no está con nosotros,usted es nuestro madre y padre,\nen el cual nosotros la queremos\ny amamos todos los días.",
       },
 
       // ════════════════════════════════════════════════════════
